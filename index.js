09b6eab61de7db53190e02fa653a94ab7debcb42
Faizal Luthfi <zalluth@gmail.com>
2020-08-22 23:22:43 +0000

    update labels

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   console.log(req.body);
   const uid = req.body.uid;
-  console.log(`Received token ${uid}.`);
+  console.log(`Received uid ${uid}.`);
 
   admin.auth().createCustomToken(uid)
     .then(function(customToken) {
       res.send(customToken);
     })
     .catch(function(error) {
-      console.log(`Error storing token ${uid}.`, error);
-      res.send(`Error storing token ${uid}.`);
+      console.log(`Error storing uid ${uid}.`, error);
+      res.send(`Error storing uid ${uid}.`);
     });
 });
 
