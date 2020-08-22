a56e170cdfb1104fd6ce5f1c8d7f62c9d3f0d209
Faizal Luthfi <zalluth@gmail.com>
2020-08-22 18:01:18 +0000

    implement main code

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
 });
+
+app.post('/', (req, res) => {
+  const token = req.body.token;
+  console.log(`Received token ${token}.`);
+
+  let uid = 'some-uid';
+
+  admin.auth().createCustomToken(uid)
+    .then(function(customToken) {
+      res.send(token);
+    })
+    .catch(function(error) {
+      console.log(`Error storing token ${token}.`, error);
+    });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
